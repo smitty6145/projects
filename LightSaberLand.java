5395cd2416cf22968734bae65eac09fd4c598323
smitty6145 <45835333+smitty6145@users.noreply.github.com>
2018-12-12 19:54:07 -0500

    Add files via upload

---
+++ LightSaberLand.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lightsaber;
+
+/**
+ *
+ * @author brandon.smith1114
+ */
+public class LightSaberLand {
+    
+    public static void main(String[] args){
+       
+        String[] colors = new String[6];
+        
+        colors[0] = "Blue";
+        colors[1] = "Green";
+        colors[2] = "Yellow";
+        colors[3] = "Purple";
+        colors[4] = "Red";
+        colors[5] = "Black";
+        
+    } // close main
+    
+} // close class
